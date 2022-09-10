0e083679569f399b7fbaae193c509a22eb66d277
grinspins <sebastian.mainberger@gmail.com>
2022-09-10 18:03:53 +0200

    Add temperature probe.

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -1,21 +1,18 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
 #include "ESP8266TimerInterrupt.h"
-#include <ESP8266HTTPClient.h>
+#include <Adafruit_MAX31865.h>
+#include <Wire.h>
 #include <credentials.h>
-#include "wifi.h"
 #include "heater.h"
+#include "http.h"
 
 #define HEATER1_PIN 2
 #define HEATER2_PIN 0
 #define AC_FREQUENCY 50
 #define POST_FREQUENCY 0.2
-#define ssid ""
-
-typedef struct State {
-  float temperature;
-  bool pump_state;
-} State;
+#define CS_PIN 15
+#define PT_100_RREF 430.0
+#define PT_100_RNOMINAL 100.0
 
 volatile uint8_t gv_heater_duty_cycle = 0;
 volatile uint8_t gv_heater_cycle_count = 0;
@@ -30,10 +27,8 @@ State is_state = {
   .pump_state = false
 };
 
-WiFiEventHandler gotIpEventHandler, disconnectedEventHandler;
 ESP8266Timer ITimer;
-WiFiClient client;
-HTTPClient http;
+Adafruit_MAX31865 pt100 = Adafruit_MAX31865(CS_PIN);
 
 // TODO probably also put into heater.h
 void setHeaTerDutyCycle(uint8_t duty_cycle) {
@@ -43,11 +38,10 @@ void setHeaTerDutyCycle(uint8_t duty_cycle) {
   gv_heater_duty_cycle = duty_cycle;
 }
 
-void IRAM_ATTR TimerHandler()
+void IRAM_ATTR SSRHandler()
 {
   gv_heater_cycle_count++;
   const uint8_t cur_duty_cyle = gv_heater_cycle_count * 2;
-  // Serial.println(cur_duty_cyle);
   if (cur_duty_cyle == gv_heater_duty_cycle) {
     heatersOff();
   } 
@@ -57,25 +51,9 @@ void IRAM_ATTR TimerHandler()
   }
 }
 
-void IRAM_ATTR EnablePost()
+void IRAM_ATTR PostEnabler()
 {
   gv_do_post = true;
-}
-
-void StatusUpdate()
-{
-  http.begin(client, "http://192.168.0.169:5000/mcu");
-  http.addHeader("Content-Type", "application/octet-stream");
-  uint8_t payload[5];
-  memcpy(payload, &is_state.temperature, 4);
-  payload[4] = (uint8_t) is_state.pump_state;
-  int httpResponseCode = http.POST(payload, sizeof payload);
-  if (httpResponseCode > 0) {
-    String response = http.getString();
-    memcpy(&set_state.temperature, &response, 4);
-    set_state.pump_state = (bool) response[4];
-  }
-  http.end();
 }
 
 
@@ -83,29 +61,22 @@ void setup() {
   setUpHeater(HEATER1_PIN);
   setUpHeater(HEATER2_PIN);
 
-  gotIpEventHandler = WiFi.onStationModeGotIP([](const WiFiEventStationModeGotIP& event)
-  {
-    heatersOn();
-  });
-
-  disconnectedEventHandler = WiFi.onStationModeDisconnected([](const WiFiEventStationModeDisconnected& event)
-  {
-    heatersOff();
-  });
-
   connectWifi(WIFI_SSID, WIFI_PW);
-  Serial.begin(115200);
+  // Serial.begin(115200);
+	// Serial.setDebugOutput(true);
+  pt100.begin(MAX31865_3WIRE);
 
-	Serial.setDebugOutput(true);
-  ITimer.attachInterrupt(AC_FREQUENCY, TimerHandler);
-  ITimer.attachInterrupt(POST_FREQUENCY, EnablePost);
+  ITimer.attachInterrupt(AC_FREQUENCY, SSRHandler);
+  ITimer.attachInterrupt(POST_FREQUENCY, PostEnabler);
 }
 
 void loop() {
   if (gv_do_post) {
-    StatusUpdate();
+    StatusUpdate(is_state, set_state);
     gv_do_post = false;
   }
+
   digitalWrite(HEATER1_PIN, set_state.pump_state);
   is_state.pump_state = set_state.pump_state;
+  is_state.temperature = pt100.temperature(PT_100_RNOMINAL, PT_100_RREF);  
 }
